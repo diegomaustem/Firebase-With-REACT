6ee3d102519cc5fee25fc04d9e0ff9c86ebdc076
Diego Maustem <diego.maustem@gmail.com>
2022-03-31 14:50:19 -0300

    trabalhando-com-listas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './style.css';
+import firebase from "./firebaseConnection";
 
 function App() {
+  const [titulo, setTitulo] = useState('');
+  const [autor, setAutor] = useState('');
+  
+
+  async function handleAdd(){
+
+    if(titulo.length === 0 || autor.length === 0){
+        alert('Erro, não foi possivel realizar a inserção!')
+        return;
+    }
+    
+    await firebase.firestore().collection('posts')
+    .add({
+      titulo: titulo,
+      autor: autor
+    })
+    /*.doc('bmu5EIJxmPEHhPzEvNt4')
+    .set({
+        titulo: titulo,
+        autor: autor
+    })*/
+    .then(()=>{
+      alert('Dados casdatrados com sucesso!')
+    })
+    .catch((error)=>{
+      alert('Ops, foi gerado algum error!' + error)
+    })
+
+  }
+
+  async function searchPost(){
+
+    await firebase.firestore().collection('posts')
+    .doc('bmu5EIJxmPEHhPzEvNt4')
+    .get()
+    .then((snapshot)=>{
+
+      setTitulo(snapshot.data().titulo);
+      setAutor(snapshot.data().autor);
+
+    })
+    .catch((error)=>{
+      alert('Ops, foi gerado algum error!' + error)
+    })
+
+    
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+          <h1>FIRE + REACT</h1><br/>
+          <div className='container'>
+            <label>Título: </label>
+            <textarea type='text' value={titulo} onChange={ (e) => setTitulo(e.target.value)}/>
+
+            <label>Autor: </label>
+            <textarea type='text' value={autor} onChange={ (e) => setAutor(e.target.value)}/><br/>
+
+            <button onClick={ handleAdd }>Cadastrar</button><br/>
+            <button onClick={ searchPost }>Buscar Post</button><br/>
+
+            <ul>
+                {posts.map((post)=>{
+                    return(
+                      <li key={post.id}>
+                          <span>Título: {post.titulo}</span><br/>
+                          <span>Autor: {post.autor}</span><br/>
+                      </li>
+                    )
+                })}
+            </ul>
+
+          </div>          
     </div>
   );
 }
---
+++ src/firebaseConnection.js
@@ -0,0 +1,20 @@
+import firebase from "firebase/app";
+import 'firebase/firestore';
+
+let firebaseConfig = {
+    apiKey: "AIzaSyCtPI7be7yFlkAWCQOMXpXXDEyXxtIBhrg",
+    authDomain: "curso-72c30.firebaseapp.com",
+    projectId: "curso-72c30",
+    storageBucket: "curso-72c30.appspot.com",
+    messagingSenderId: "815031405",
+    appId: "1:815031405:web:0139cfe5426cf806790b64",
+    measurementId: "G-330X5FC41E"
+  };
+
+
+  if(!firebase.apps.length){
+    firebase.initializeApp(firebaseConfig);
+  }
+
+  export default firebase;
+  
